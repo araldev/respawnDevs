f0647262f07fc7eb694d2ebc0f6cc8123a420fc4
ZaneDev <alvferbarrero@gmail.com>
2026-05-20 13:45:26 +0200

    Nuevos imports

--- backend/src/main/java/com/example/RespawnDevs/Repositorios/AlergenoRepository.java
+++ backend/src/main/java/com/example/RespawnDevs/Repositorios/AlergenoRepository.java
@@ -1,5 +1,7 @@
 package com.example.RespawnDevs.Repositorios;
 
+import java.util.List;
+
 import com.example.RespawnDevs.Entidades.Alergenos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- backend/src/main/java/com/example/RespawnDevs/Repositorios/IngredienteRepository.java
+++ backend/src/main/java/com/example/RespawnDevs/Repositorios/IngredienteRepository.java
@@ -4,6 +4,7 @@ import com.example.RespawnDevs.Entidades.Ingredientes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
